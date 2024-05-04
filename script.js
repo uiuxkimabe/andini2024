457cc18428bb5d0c7dd2afd3b79b80c36a1a1fa7
uiuxkimabe <uiuxkimabe@gmail.com>
2024-05-05 02:36:46 +0700

    perintah tap

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ card.addEventListener('click', () => {
   setTimeout(() => {
       hiddenMsglink.style.visibility = 'visible';
       hiddenMsg.style.display = 'flex'
-      alert('tap sembarang pada background hitam')
+      alert('tap tap pada bola putih')
     }, 44000);
 })
 
